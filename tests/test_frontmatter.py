ad719b26fb93db747bf741598822e90886d50753
Maxime Filippini <maxime.filippini@gmail.com>
2024-02-15 08:08:40 +0100

    fix triple quote blocks in tests

--- tests/test_frontmatter.py
+++ tests/test_frontmatter.py
@@ -5,14 +5,14 @@ from new_site.build import parse_yaml_frontmatter
 
 @fixture
 def no_frontmatter():
-    return """
+    return """\
 hello
 """
 
 
 @fixture
 def incorrect_frontmatter():
-    return """
+    return """\
 ---
 title: hello
 --
@@ -22,7 +22,7 @@ hi
 
 @fixture
 def mismatch_fences():
-    return """
+    return """\
 -------
 title: hello
 ---
@@ -32,19 +32,19 @@ hi
 
 
 def test_no_frontmatter(no_frontmatter):
-    frontmatter, content = parse_yaml_frontmatter(no_frontmatter[1:])
+    frontmatter, content = parse_yaml_frontmatter(no_frontmatter)
     assert frontmatter is None
     assert content.strip() == "hello"
 
 
 def test_incorrect_frontmatter(incorrect_frontmatter):
-    frontmatter, content = parse_yaml_frontmatter(incorrect_frontmatter[1:])
+    frontmatter, content = parse_yaml_frontmatter(incorrect_frontmatter)
     assert frontmatter is None
     assert content.strip() == incorrect_frontmatter.strip()
 
 
 def test_mismatch_fences(mismatch_fences):
-    frontmatter, content = parse_yaml_frontmatter(mismatch_fences[1:])
+    frontmatter, content = parse_yaml_frontmatter(mismatch_fences)
 
     assert isinstance(frontmatter, dict)
     assert "title" in frontmatter
