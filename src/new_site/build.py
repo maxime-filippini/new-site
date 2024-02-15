27249d62c1c69c93ea52f492208d7958c8a0377e
Maxime Filippini <maxime.filippini@gmail.com>
2024-02-15 08:07:47 +0100

    fix tests

--- src/new_site/build.py
+++ src/new_site/build.py
@@ -5,26 +5,26 @@ from typing import Any
 
 import yaml
 
-RE_FRONTMATTER = r"^(?:---\s*\n)((?:.*\n)*?)(?:---\s*\n)"
+RE_FRONTMATTER = r"^(?:-{3,}\s*\n)((?:.*\n)*?)(?:-{3,}\s*\n)(.+)$"
 
 
-def parse_yaml_frontmatter(text: str) -> Any | None:
+def parse_yaml_frontmatter(text: str) -> tuple[Any | None, str]:
     """Parse the frontmatter for a blog post.
 
     Args:
         text (str): Contents of the markdown file.
 
     Returns:
-        dict[str, Any] | None: Metadata or None if no frontmatter was provided.
+        tuple[Any | None, str]: Frontmatter, content.
     """
     match = re.match(RE_FRONTMATTER, text)
 
     if not match:
-        return None
+        return None, text
 
     groups = match.groups()
 
     if not groups:
-        return None
+        return None, text
 
-    return yaml.safe_load(match.groups()[0])
+    return yaml.safe_load(match.groups()[0]), groups[1]
--- tests/test_frontmatter.py
+++ tests/test_frontmatter.py
@@ -4,10 +4,49 @@ from new_site.build import parse_yaml_frontmatter
 
 
 @fixture
-def test_file():
-    with open("tests/fixtures/frontmatter.md", "r") as fd:
-        return fd.read()
+def no_frontmatter():
+    return """
+hello
+"""
 
 
-def test_parsing(test_file):
-    parse_yaml_frontmatter(test_file)
+@fixture
+def incorrect_frontmatter():
+    return """
+---
+title: hello
+--
+hi
+"""
+
+
+@fixture
+def mismatch_fences():
+    return """
+-------
+title: hello
+---
+
+hi
+"""
+
+
+def test_no_frontmatter(no_frontmatter):
+    frontmatter, content = parse_yaml_frontmatter(no_frontmatter[1:])
+    assert frontmatter is None
+    assert content.strip() == "hello"
+
+
+def test_incorrect_frontmatter(incorrect_frontmatter):
+    frontmatter, content = parse_yaml_frontmatter(incorrect_frontmatter[1:])
+    assert frontmatter is None
+    assert content.strip() == incorrect_frontmatter.strip()
+
+
+def test_mismatch_fences(mismatch_fences):
+    frontmatter, content = parse_yaml_frontmatter(mismatch_fences[1:])
+
+    assert isinstance(frontmatter, dict)
+    assert "title" in frontmatter
+    assert frontmatter["title"] == "hello"
+    assert content.strip() == "hi"
